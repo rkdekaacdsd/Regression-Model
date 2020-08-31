308de7e4f54f1e572a82cbcb935b9b22f48029a4
RKDeka <rkdekaacdsd@gmail.com>
2020-08-31 20:30:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+
+    #Back end calculations - Predictions
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+
+    #Showing result for Classification
+    return render_template('index.html', prediction_text='Expected Gross Calory Value is {} KCal'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
